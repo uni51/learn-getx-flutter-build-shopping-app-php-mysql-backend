b93208170c3c65f68e1b95a43d124e185c429def
uni51 <unieye51@gmail.com>
2023-04-16 14:08:15 +0900

    3. setup getx in our project

---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:clothes_app/users/authentication/login_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+void main()
+{
+  WidgetsFlutterBinding.ensureInitialized();
+
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      title: 'Clothes Store App',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.purple,
+      ),
+      home: FutureBuilder(
+        builder: (context, dataSnapShot)
+        {
+          return LoginScreen();
+        },
+      ),
+    );
+  }
+}
